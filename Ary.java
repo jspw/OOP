8ca72ad682ddabfef90214fae7cd931ce5f11676
MH_Shifat <mehedi17@student.sust.edu>
2019-03-27 10:28:15 +0600

    Update Ary.java

--- Ary.java
+++ Ary.java
@@ -1,4 +1,15 @@
-class Ary{
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javathreads;
+
+/**
+ *
+ * @author jacksparrow
+ */
+public class Ary{
     int[] aray3 = {11, 12, 13, 14, 15};
     int[] aray4 = {16, 17, 18, 19, 20};
     int[] sum2=new int[5];
