b354ae1dde55952004857aa49161775973a447de
MH_Shifat <mehedi17@student.sust.edu>
2019-03-27 10:28:58 +0600

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,14 @@
 /*
-     @author JackSparrow
-*/
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javathreads;
 
+/**
+ *
+ * @author jacksparrow
+ */
 /*
 4 arrays !
 Add 1st 2 arrays in a another sum1 array
@@ -19,7 +26,7 @@ public class main {
         int[] aray2 = {6, 7, 8, 9, 10};
         int[] sum1=new int[5];
         int[] finalsum=new int[5];
-        Sum obj = new Sum();
+        doSum obj = new doSum();
         Thread a=new Thread(obj);
         a.start();
         for (int i = 0; i < 5; i++) {
@@ -35,7 +42,7 @@ public class main {
         } catch (InterruptedException e) {
            
         }
-        System.out.println("Summation Array of four arrays:");
+        System.out.println("Sum Array of four arrays:");
         for(int i=0;i<5;i++)
         {
             finalsum[i]=sum1[i]+obj.sum2[i];
