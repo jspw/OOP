31e1543ed2044ac176582298d2dd56e26ef79ba0
MH_Shifat <mehedi17@student.sust.edu>
2019-03-27 10:28:37 +0600

    Update doSum.java

--- doSum.java
+++ doSum.java
@@ -1,4 +1,15 @@
-class doSum extends A implements Runnable {
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javathreads;
+
+/**
+ *
+ * @author jacksparrow
+ */
+public class doSum extends Ary implements Runnable {
 
     @Override
     public void run() {
